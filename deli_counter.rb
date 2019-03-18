1fa8db6cb1204201193342392f62e702f902d1b5
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-18 03:36:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,11 @@ def line(katz_deli)
 if katz_deli.length == 0 
   puts "The line is currently empty."
   else
-linenumber = "The line is currently:"
+line_number = "The line is currently:"
 katz_deli.each.with_index(1)  do |name, i|
-  linenumber << " #{i}.#{name}"
+  line_number << " #{i}. #{name}"
 end
-puts linenumber
+puts line_number
 end
 end
 
